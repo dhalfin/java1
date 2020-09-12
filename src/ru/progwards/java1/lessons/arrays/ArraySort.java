47a16bda2782f7e3aae76c36090d5bcfde64bf2d
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-09-12 20:59:49 +0300

    updated sort method in class ArraySort

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -7,7 +7,7 @@ public class ArraySort {
     public static void sort(int[] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
-                if (a[i] < a[j]) {
+                if (a[i] > a[j]) {
                     int temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
