87ddd14675987d12a7abec71bf2e2e8c40554c50
Dinar Khalfin <dinarhalfin@yahoo.com>
2020-09-15 18:48:17 +0300

    updated toString() method in class Binary

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -10,7 +10,7 @@ public class Binary {
     public String toString() {
         int n = num & 0x7F_FF_FF_FF;
         String result = num < 0 ? "1" : "0";
-        for (int i = 1; i < 32; i++) {
+        for (int i = 1; i < 8; i++) {
             result += (n & 0x40_00_00_00) == 0x40_00_00_00 ? "1" : "0";
             n <<= 1;
         }
